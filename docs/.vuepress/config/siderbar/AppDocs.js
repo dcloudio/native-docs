ea476d8ea541c4decd0a433ea63047a2d035faa8
DCloud_LXH <283700113@qq.com>
2023-02-09 10:34:00 +0800

    feat: 更新QQ群

---
+++ docs/.vuepress/config/siderbar/AppDocs.js
@@ -0,0 +1,32 @@
+export default {
+  contactItems: [
+  ],
+  codeHosting: [
+  ],
+  qq_group: [
+    {
+      "number": "256775471",
+      "state": 0,
+      "prefix": "群1",
+      joinQQGroupHref: 'https://qm.qq.com/cgi-bin/qm/qr?k=R1Rnbxs1l6N4rWBaogiALGTJfFp2-ulP&jump_from=webapi&authKey=PrQBXSCArDfHQ3JEztYModlZtkfOJCkWI4zB/wERWE/Kld6sipkB1EqJ2TLbAi0h'
+    },
+    {
+      "number": "814228233",
+      "state": 0,
+      "prefix": "群2",
+      joinQQGroupHref: 'https://qm.qq.com/cgi-bin/qm/qr?k=uwr3DhuW8Gpc5aUsurquozgezy1EmGFj&jump_from=webapi&authKey=/Vv1/nXAT9b9szQEKkhOyZWB9dv8VWyE7nq6xMUIpP1lj/vwkbPfghMIxSTO/Tac'
+    },
+    {
+      "number": "455763866",
+      "state": 0,
+      "prefix": "群3",
+      joinQQGroupHref: 'https://qm.qq.com/cgi-bin/qm/qr?k=X-XQEy3cHAaFHFXhQLrDJbSlPZDug78e&jump_from=webapi&authKey=4xuwh2OdNvyPUX6/tTN81gu3WkzhYdaKfEwFd73HY5DE6pq84EKy44n0Wzuo+CzZ'
+    },
+    {
+      "number": "741406073",
+      "state": 0,
+      "prefix": "群4",
+      joinQQGroupHref: 'https://qm.qq.com/cgi-bin/qm/qr?k=V1NOZh7HCJiPxF_kTjfN9zAGDcKtK74X&jump_from=webapi&authKey=YITNfNnLrVMtt/sfpzDWewYIa1QdkE3LEmh5SaKTE55fYF5wm4ZtdxU8nMtUqR5O'
+    }
+  ]
+}
