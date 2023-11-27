54655ed2d3edac3fe46b05c5bedd21b938c91aad
DCloud_LXH <283700113@qq.com>
2023-11-27 20:29:16 +0800

    fix: footer.js

--- docs/.vuepress/config/en/footer.js
+++ docs/.vuepress/config/en/footer.js
@@ -162,7 +162,7 @@ export default {
 		  subTitle: 'acceptable use policy',
 		  url: 'https://account.dcloud.io/privacy-policy/DCloud-Acceptable-Use-Policy.html',
 		}
-        
+
       ],
     },
     {
@@ -179,6 +179,45 @@ export default {
       ],
     },
   ],
+  aboutus: {
+    title: 'About',
+    content: [
+      {
+        subTitle: 'DCloud',
+        url: 'https://dcloud.io/',
+      },
+      {
+        subTitle: 'Jobs',
+        url: 'https://www.dcloud.io/hr/',
+      },
+      {
+        subTitle: 'Terms of service',
+        url: 'https://account.dcloud.io/privacy-policy/DCloud-Terms-of-Service.html',
+      },
+      {
+        subTitle: 'Privacy policy',
+        url: 'https://account.dcloud.io/privacy-policy/DCloud-Privacy-Policy.html',
+      },
+      {
+        subTitle: 'acceptable use policy',
+        url: 'https://account.dcloud.io/privacy-policy/DCloud-Acceptable-Use-Policy.html',
+      }
+
+    ],
+  },
+  concatus: {
+    title: 'Contact us',
+    content: [
+      {
+        subTitle: 'bussiness：bd@dcloud.io',
+        url: 'mailto:bd@dcloud.io',
+      },
+      {
+        subTitle: 'Advertising：uniad@dcloud.io',
+        url: 'mailto:uniad@dcloud.io',
+      },
+    ],
+  },
   domainImg: 'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f184e7c3-1912-41b2-b81f-435d1b37c7b4/769929a3-65eb-4d11-815d-84f88197a152.png',
   beian: 'http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802035340'
 }
